f401d739c77929516daeac689699f16cea1521b5
jam <jamalabdulov@mail.ru>
2024-12-27 23:25:13 +0300

    Initial Commit

---
+++ AbdulovDO_2048/AbdulovDO_2048App.swift
@@ -0,0 +1,17 @@
+//
+//  AbdulovDO_2048App.swift
+//  AbdulovDO_2048
+//
+//  Created by Jam on 27.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AbdulovDO_2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
